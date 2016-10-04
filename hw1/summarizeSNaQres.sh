ad76761e9cf4a63f6ba35022918f05107d46998f
pgonzalez6 <pgonzalez@wisc.edu>
2016-10-03 23:02:49 -0500

    changes

--- hw1/summarizeSNaQres.sh
+++ hw1/summarizeSNaQres.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
 
+### At first get hmax from each file
 for i in {1..13};do 
 grep "hmax" log/timetest${i}_snaq.log 
 grep "hmax" out/timetest${i}_snaq.out	
+
+### print the CPU time 
+ps -e -o %cpu | awk '{s+=$1} END {print s}' 
+>outputs.csv
 done
 
 
