b2a042b5444946189e8a18ca6373f4033f12f761
pgonzalez6 <pgonzalez@wisc.edu>
2016-10-06 15:41:55 -0500

    change

--- hw1/summarizeSNaQres.sh
+++ hw1/summarizeSNaQres.sh
@@ -6,16 +6,21 @@
 
 ### At first get hmax from each file
 
-echo -n > out1.csv
+
 for i in log/*.log;
 do
 	echo "i=$i" 
 	analysis=$(echo $i | grep -o -E "[^/]+\.log" | grep -o -E "[^.log]+")
 	h=$(grep "hmax" $i | head -n1 | grep -o -E "[0-9]")
-	CPU_Time=$(grep "Elapsed time" out/analysis.out)	
+	#CPU=$(grep -o -E "Elapsed time. \d+" out/*)	
 	echo "h=$h ; analysis=$analysis"
-echo "$h,$analysis" >> out1.csv	
+
 done
-
-
+for i in out/*.out;
+do
+	echo "i=$i" 
+	CPU=$(grep -E "Elapsed time. \d+" -o out/t*)	
+	echo "CPU=$CPU"
+echo "$h,$analysis,$CPU" >> out5.csv		
+done
 
