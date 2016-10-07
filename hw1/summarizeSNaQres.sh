60814179b0d28882f34a45a62c31ca810d17c03a
pgonzalez6 <pgonzalez@wisc.edu>
2016-10-07 11:34:07 -0500

    changes ex3

--- hw1/summarizeSNaQres.sh
+++ hw1/summarizeSNaQres.sh
@@ -1,28 +1,16 @@
 #!/bin/bash 
 
-### Get the root
-
-
-
-### At first get hmax from each file
-
-echo analysis,Hmax,cputime > out5.csv
+echo analysis,Hmax,CPU,xtolAbs > output.csv
 for i in log/*.log;
 do
 	echo "i=$i" 
 	analysis=$(echo $i | grep -o -E "[^/]+\.log" | grep -o -E "[^.log]+")
 	h=$(grep "hmax" $i | head -n1 | grep -o -E "[0-9]")
 	CPU=$(grep -E "Elapsed time. \d+" -o out/$analysis.out)	
-	echo "analysis=$analysis ; h=$h ; CPU=$CPU"
-    echo "$analysis,$h,$CPU" >> out5.csv	
+	xtolAbs=$(grep "xtolAbs" $i | head -n1 | grep -o -E "[0-9]+")
+	echo "analysis=$analysis ; h=$h ; CPU=$CPU ; xtolAbs=$xtolAbs"
+    echo "$analysis,$h,$CPU,$xtolAbs" >> output.csv	
 done
 
 
-#for i in out/*.out;
-#do
-#	echo "i=$i" 
-#	CPU=$(grep -E "Elapsed time. \d+" -o out/t*)	
-#	echo "CPU=$CPU"
-	
-#done
 
