13b34645df5b253690a4c09311410d29e2189cb5
pgonzalez6 <pgonzalez@wisc.edu>
2016-10-09 23:30:36 -0500

    change

--- hw1/summarizeSNaQres.sh
+++ hw1/summarizeSNaQres.sh
@@ -7,10 +7,10 @@ do
 	analysis=$(echo $i | grep -o -E "[^/]+\.log" | grep -o -E "[^.log]+")
 	h=$(grep "hmax" $i | head -n1 | grep -o -E "[0-9]")
 	CPU=$(grep -E "Elapsed time. \d+" -o out/$analysis.out)	
-	xtolAbs=$(grep "xtolAbs" $i | head -n1 | grep -o -E "[0-9]+")
-	echo "analysis=$analysis ; h=$h ; CPU=$CPU ; xtolAbs=$xtolAbs"
-    echo "$analysis,$h,$CPU,$xtolAbs" >> output.csv	
+	echo "analysis=$analysis ; h=$h ; CPU=$CPU "
+    echo "$analysis,$h,$CPU" >> output.csv	
 done
 
 
+##	xtolAbs=$(grep "xtolAbs" $i | head -n1 | grep -o -E "[0-9]+")
 
