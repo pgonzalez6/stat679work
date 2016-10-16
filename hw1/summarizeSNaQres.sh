7dfefaa9c5c48826484ebed4d99d664f01add36a
pgonzalez6 <pgonzalez@wisc.edu>
2016-10-16 11:24:37 -0500

    changes

--- hw1/summarizeSNaQres.sh
+++ hw1/summarizeSNaQres.sh
@@ -1,16 +1,26 @@
 #!/bin/bash 
 
-echo analysis,Hmax,CPU> output.csv
+echo analysis,Hmax,CPU,Nruns,Nfails,fabs,frel,xtolAbs,xtolRel,Seed> output.csv
 for i in log/*.log;
 do
 	echo "i=$i" 
 	analysis=$(echo $i | grep -o -E "[^/]+\.log" | grep -o -E "[^.log]+")
 	h=$(grep "hmax" $i | head -n1 | grep -o -E "[0-9]")
 	CPU=$(grep -E "Elapsed time. \d+" -o out/$analysis.out)	
-	echo "analysis=$analysis ; h=$h ; CPU=$CPU "
-    echo "$analysis,$h,$CPU" >> output.csv	
+	Nruns=$(grep -o -E "BEGIN: *[0-9]." $i | head -n1 | grep -o -E "[0-9]+") 
+	Nfails=$(grep "max number of failed proposals" $i | cut -d , -f1 | grep -o -E "[0-9]+")
+	fabs=$(grep -E "ftolAbs" $i | head -n1 | cut -d , -f2 | grep -E -o "[-+]?[0-9]*\.?[0-9]+([eE][-+]?[0-9]+)?")
+	frel=$(grep -E "ftolRel" $i | head -n1 | cut -d , -f1 | grep -E -o "[-+]?[0-9]*\.?[0-9]+([eE][-+]?[0-9]+)?")
+	xtolAbs=$(grep -E "xtolAbs" $i | head -n1 | cut -d , -f1 | grep -E -o "[0-9]\.[0-9]+")
+	xtolRel=$(grep -E "xtolRel" $i | head -n1 | cut -d , -f2 | grep -E -o "[0-9]\.[0-9]+")
+	Seed=$(sed -n '9p' $i | head -n1 | grep -o -E "[0-9]+")
+	#under3460=$(sed -nE 's/.*-loglik=([0-9,]+).*/\1/p' log/*.log | for i in log/*.log do if [ $i -lt 3460 ] then echo "A" else echo "B" fi done)
+	echo "analysis=$analysis ; h=$h ; CPU=$CPU ; Nruns=$Nruns ; Nfails=$Nfails ; fabs=$fabs ; frel=$frel ; xtolAbs=$xtolAbs ; xtolRel=$xtolRel ; Seed=$Seed "
+    echo "$analysis,$h,$CPU,$Nruns,$Nfails,$fabs,$frel,$xtolAbs,$xtolRel,$Seed" >> output.csv	
+
 done
 
 
-##	xtolAbs=$(grep "xtolAbs" $i | head -n1 | grep -o -E "[0-9]+")
+
+
 
