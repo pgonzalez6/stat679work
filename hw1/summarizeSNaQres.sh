2fe9657bee8b17884e37289fc0bdc0b7f529d845
Pablo González Barrios <pgonza2211@gmail.com>
2016-10-18 00:11:49 -0500

    Update summarizeSNaQres.sh

--- hw1/summarizeSNaQres.sh
+++ hw1/summarizeSNaQres.sh
@@ -14,10 +14,7 @@ do
 	xtolAbs=$(grep -E "xtolAbs" $i | head -n1 | cut -d , -f1 | grep -E -o "[0-9]\.[0-9]+")
 	xtolRel=$(grep -E "xtolRel" $i | head -n1 | cut -d , -f2 | grep -E -o "[0-9]\.[0-9]+")
 	Seed=$(sed -n '9p' $i | head -n1 | grep -o -E "[0-9]+") ## Use of sed function to take information from line 9 and thhen grep the seed number
-#My objective of this part is to get the information loglik using sed and then generate a loop with functions for and if you allow me to classify each of 
-#the values in smaller or greater than 3460, 3450 and 3460 (something like if [$ i -lt 3460] then I echo "1" else echo "0" fi]). This way you could get a 
-#new variable from 0 and 1, by adding the 1's could get the number of "successes" in each row. The first part works fine and I get all loglik values per row,
-#however I am having trouble posting the required information. I appreciate if someone can help me with this.
+#the objective here is generate the count variables for under3440,3450 and 3460
     sed -nE 's/.*-loglik=([0-9,]+).*/\1/p' $i > t1.txt
 	under3440=0
     under3450=0
